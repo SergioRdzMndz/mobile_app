d96c5d88d272e03334f82f733ff213adc421a737
SergioRdzMndz <utm24090656@utma.edu.mx>
2026-03-05 10:09:34 -0600

    Nuevas rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
+   {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
+  },
   {
     path: '',
     redirectTo: 'home',
